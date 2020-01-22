9ed13e587741bc5a49b81183658ce4c92b03359c
Marcin Jastrzębski <jaczomb@gmail.com>
2020-01-22 22:25:09 +0100

    Added some tests for classes Calculator and SimpleUser

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        // test klasy simpleUser
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test user OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // test klasy Calculator - 1.dodawanie
+        Calculator calculator = new Calculator();
+        Integer result2 = calculator.add(2, 5);
+        if (result2.equals(7)) {
+            System.out.println("Dodawanie OK");
+        } else {
+            System.out.println("Dodawanie Error!");
+        }
+
+        // test klasy Calculator - 2.odejmowanie
+        Calculator calculator2 = new Calculator();
+        Integer result3 = calculator2.subtract(2, 5);
+        if (result3.equals(-3)) {
+            System.out.println("Odejmowanie OK");
+        } else {
+            System.out.println("Odejmowanie Error!");
+        }
+
     }
 }
