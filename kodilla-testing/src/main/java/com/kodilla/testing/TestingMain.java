a19ee66870bddd4ceac5f2eceb72c0ef9527fecb
Marcin Jastrzębski <jaczomb@gmail.com>
2020-01-27 23:55:03 +0100

    main function has been cleared

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,14 +5,9 @@ import com.kodilla.testing.collection.OddNumberExterminator;
 import java.util.ArrayList;
 
 public class TestingMain {
+
     public static void main(String[] args) {
-        ArrayList<Integer> lista = new ArrayList<Integer>();
-        lista.add(4);
-        lista.add(5);
-        lista.add(6);
-        lista.add(7);
-        System.out.println(lista);
-        System.out.println(OddNumberExterminator.exterminate(lista));
 
     }
+
 }
