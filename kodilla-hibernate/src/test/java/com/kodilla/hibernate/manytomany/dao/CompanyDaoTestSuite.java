706d93344f64a72680d0d14a80fcd0147ddcb1b5
Marcin Jastrzębski <marcin.jastrzebski@outlook.com>
2020-05-21 19:36:24 +0200

    17.4 the main task

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany(){
@@ -58,6 +62,61 @@ public class CompanyDaoTestSuite {
             companyDao.deleteById(greyMatterId);
         } catch (Exception e) {
             //do nothing
+        }
+    }
+
+    @Test
+    public void testNamedNativeQuery(){
+        //Given
+        Company company1 = new Company("Kodilla");
+        Company company2 = new Company("Kodzilla");
+        Company company3 = new Company("Microsoft");
+        Company company4 = new Company("Motorolla");
+
+        Employee employee1 = new Employee("Adam", "Kowalski");
+        Employee employee2 = new Employee("Michał", "Kowalski");
+        Employee employee3 = new Employee("Tomasz", "Biedrzycki");
+        Employee employee4 = new Employee("Zbigniew", "Stonoga");
+
+        company1.getEmployees().add(employee1);
+        company1.getEmployees().add(employee2);
+        company2.getEmployees().add(employee2);
+        company2.getEmployees().add(employee3);
+        company3.getEmployees().add(employee3);
+        company3.getEmployees().add(employee4);
+        company4.getEmployees().add(employee4);
+        company4.getEmployees().add(employee1);
+
+        employee1.getCompanies().add(company1);
+        employee1.getCompanies().add(company4);
+        employee2.getCompanies().add(company1);
+        employee2.getCompanies().add(company2);
+        employee3.getCompanies().add(company2);
+        employee3.getCompanies().add(company3);
+        employee4.getCompanies().add(company3);
+        employee4.getCompanies().add(company4);
+
+        companyDao.save(company1);
+        int id = company1.getId();
+        companyDao.save(company2);
+        companyDao.save(company3);
+        companyDao.save(company4);
+
+        employeeDao.save(employee1);
+        employeeDao.save(employee2);
+        employeeDao.save(employee3);
+        employeeDao.save(employee4);
+
+        //When
+        List<Company> companiesWithLetters = companyDao.retrieveCompaniesStartingWithLetters("Kod");
+        List<Employee> employeesWithGivenLastName = employeeDao.retrieveEmployeesWithGivenLastName("Kowalski");
+
+        //Then
+        try {
+            Assert.assertEquals(2, companiesWithLetters.size());
+            Assert.assertEquals(2, employeesWithGivenLastName.size());
+        } finally {
+            companyDao.deleteById(id);
         }
     }
 }
